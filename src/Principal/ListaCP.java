47945b56ff18c86e508bf9f0807d75490eb0fbb5
Kerikeion <chamaledaniel0@gmail.com>
2015-08-04 22:22:38 -0700

    Primeras tres clases, solo las clases con el minimo de logica

---
+++ src/Principal/ListaCP.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Principal;
+
+/**
+ *
+ * @author PDARML
+ */
+public class ListaCP {
+    
+    Nodo primero = null;
+    Nodo ultimo = null;
+    
+    public ListaCP(){
+    
+    }
+    
+    public void Agregar(){
+        //if(){
+        
+        //}
+    }
+    
+}
---
+++ src/Principal/Nodo.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Principal;
+
+/**
+ *
+ * @author PDARML
+ */
+public class Nodo {
+    
+    Nodo siguiente = null;
+    Nodo anterior = null;
+    String texto = "";
+    
+    public Nodo(){}
+    
+}
